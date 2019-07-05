76b73672c87f0daeba0691500ff3140179fd9fdb
aimarl21 <aimarl21-justin21-yu221@outlook.jp>
2019-07-05 23:19:19 +0900

    Lesson7

---
+++ Assets/CubeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    //消滅位置
+    private float deadLine = -10;
+
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>(); 
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine){
+            Destroy(gameObject);
+        }
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string yourTag = collision.gameObject.tag;
+        Debug.Log(yourTag + "/" + collision.gameObject.name);
+
+        if (yourTag == "block"||yourTag=="ground")
+        {
+            audio.Play();
+        }
+    }
+ 
+            
+
+     
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    //ゲームオーバーテキスト
+    private GameObject gameOverText;
+
+    //走行距離テキスト
+    private GameObject runLengthText;
+
+    //走った距離
+    private float Len = 0;
+
+    //走る速度
+    private float speed = 0.03f;
+
+    //ゲームオーバーの判定
+    private bool isGameOver=false;
+
+	// Use this for initialization
+	void Start () {
+        //シーンビューからオブジェクトの実体を検索する
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isGameOver == false)
+        {
+            //走った距離を更新する
+            this.Len += this.speed;
+
+            //走った距離を表示する
+            this.runLengthText.GetComponent<Text>().text = "Distance" + Len.ToString("F2") + "m";
+        }
+
+        //ゲームオーバーになった場合
+        if (this.isGameOver == true){
+            //クリックされたらシーンをロードする（追加）
+            if (Input.GetMouseButtonDown(0)){
+                //GameSceneを読み込む（追加）
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+    }
+
+    public void GameOver(){
+        //ゲームオーバーになったときに、画面上にゲームオーバーを表示する
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
